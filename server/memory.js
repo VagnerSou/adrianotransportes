775204a4c37c9ebd78a4e7f46fb9910cefd29fac
VagnerSou <vagnerfelixbaixa@gmail.com>
2026-05-25 19:52:12 -0300

    adrianotransportes

--- server/memory.js
+++
@@ -1,2 +0,0 @@
-export const motoristas = [];
-export const corridas = [];
--- server/server/passenger-app/passenger-app/socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("http://SEU_IP_LOCAL:3000");
-
-export default socket;
